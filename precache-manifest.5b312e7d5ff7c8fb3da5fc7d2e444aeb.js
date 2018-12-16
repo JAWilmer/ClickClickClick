d18e4376b75fda0209e626d980d381da033f09ca
Wilmer <julieannewilmer@gmail.com>
2018-12-16 17:09:38 -0600

    Updates

---
+++ precache-manifest.5b312e7d5ff7c8fb3da5fc7d2e444aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "483e48b8da769748de1a",
+    "url": "/ClickClickClick/static/js/runtime~main.483e48b8.js"
+  },
+  {
+    "revision": "fa9324c1d7e93be96df2",
+    "url": "/ClickClickClick/static/js/main.fa9324c1.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/ClickClickClick/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "fa9324c1d7e93be96df2",
+    "url": "/ClickClickClick/static/css/main.18c660da.chunk.css"
+  },
+  {
+    "revision": "750b1222a02f2a9e7e2c79f1805dd1fe",
+    "url": "/ClickClickClick/index.html"
+  }
+];
